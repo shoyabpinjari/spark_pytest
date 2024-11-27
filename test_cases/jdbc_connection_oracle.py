85e9520416e961702dc924aa660f5e180a71ccf4
Shoyab <sspinjari1510@gmail.com>
2024-11-27 10:09:53 +0530

    27Nov24 Updated from shoyab

---
+++ test_cases/jdbc_connection_oracle.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+jar = r"C:\Users\Admin\PycharmProjects\pythonProject\spark_pytest\jar\ojdbc8-21.5.0.0.jar"
+
+spark = SparkSession.builder.master("local[1]") \
+    .appName("test")\
+    .config("spark.jars", jar)\
+    .config("spark.driver.extraClassPath", jar)\
+    .config("spark.executor.extraClassPath", jar)\
+    .getOrCreate()
+
+
+df = (spark.read.format("jdbc")
+    .option("url","jdbc:oracle:thin:@//localhost:1521/XEPDB1")
+    .option("query","select * from departments")      # .option("dbtable","employees")
+    .option("user","hr")
+    .option("password","hr")
+    .option("driver","oracle.jdbc.driver.OracleDriver")
+    .load())
+
+df.show()
+
+
+## Connection
--- test_cases/write_target.py
+++ test_cases/write_target.py
@@ -6,8 +6,10 @@ spark = SparkSession.builder \
       .master("local[1]") \
       .appName("SparkByExamples.com") \
       .getOrCreate()
+
 dataList = [("Java", 20000), ("Python", 100000), ("Scala", 3000)]
-df=spark.createDataFrame(dataList, schema=['Language','fee'])
+
+df = spark.createDataFrame(dataList, schema=['Language','fee'])
 
 df.show()
 
